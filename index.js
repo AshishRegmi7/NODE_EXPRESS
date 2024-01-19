5d994343f45c8f6e155cc554a2ffa7d371d89266
Ashish <ashish.reg9@gmail.com>
2024-01-19 14:31:16 +0545

    express setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+//Printing Hello World
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello World" });
+});
+app.get("/ashish", (req, res) => {
+  res.json({ msg: "Hello Ashish ji" });
+});
+app.get("/ashish/regmi", (req, res) => {
+  res.json({ msg: "Hello Ashish Regmi ji" });
+});
+
+app.listen(8000, () => {
+  console.log("application is running");
+});
