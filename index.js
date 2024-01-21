639b21900436fd904e3e0b3f8e54ceb9ba0952e5
AshishRegmi7 <ashish.reg7@gmail.com>
2024-01-21 15:17:23 -0800

    express

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-//Printing Hello World
+//Printing Hello World using express 
 
 app.get("/", (req, res) => {
   res.json({ msg: "Hello World" });
