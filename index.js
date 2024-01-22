0ca2aaa7898de1e6bbc29ebff2cd661569e478f4
Ashish Regmi <ashish.reg7@gmail.com>
2024-01-21 17:56:45 -0800

    route

--- index.js
+++ index.js
@@ -3,17 +3,33 @@ const express = require("express");
 const app = express();
 
 //Printing Hello World using express 
-
+ //Route 1
 app.get("/", (req, res) => {
   res.json({ msg: "Hello World" });
 });
-app.get("/ashish", (req, res) => {
-  res.json({ msg: "Hello Ashish ji" });
-});
-app.get("/ashish/regmi", (req, res) => {
-  res.json({ msg: "Hello Ashish Regmi ji" });
-});
 
+//Route 2
+// app.get("/:fname/:lname", (req, res) => {
+//   const data=req.params.fname;
+//   const data2=req.params.lname;
+
+//   res.json({ msg: `Hello ${data} ${data2}` });
+// });
+
+//Route 3
+
+app.get("/:data1/:data2",(req,res)=>{
+  const {data1,data2}=req.params;
+  const result=Number(data1)+Number(data2);
+  res.json({msg:`The sum is ${result}`});
+})
 app.listen(8000, () => {
   console.log("application is running");
 });
+
+//get => server to frontend
+//post => frontend to server
+//put => frontend to server; fix majority property of data
+//patch => frontend to server fix single property of data
+
+//1-16 random number generation using callback function
