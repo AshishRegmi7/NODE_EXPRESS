4044acb79e81e9936f6f25d9adc84bd639af7710
Ashish Regmi <ashish.reg7@gmail.com>
2024-01-24 17:46:32 +0545

    routes

--- routes/book.route.js
+++ routes/book.route.js
@@ -1,13 +1,19 @@
 const router=require("express").Router();
-
+const {numConverter}=require("../utils/library");
 router.get("/",(req,res)=>{
     res.json("Hello From book section")
 })
- router.post("/:id",(req,res)=>{
-       console.log(req.params);
-        console.log(req.query);
-        console.log(req.body);
-    })
+//  router.post("/:id",(req,res)=>{
+//        console.log(req.params);
+//         console.log(req.query);
+//         console.log(req.body);
+//     })
+    router.post("/:number",(req,res)=>{
+        const result=numConverter(
+            req.query.number || req.body.number || req.params.number
+        );
+    res.json({msg:result});
+     })
 
 
 module.exports=router;
