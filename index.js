f0990b0fb6c760880cc48967b89f8da31948b980
Ashish Regmi <ashish.reg7@gmail.com>
2024-01-29 15:58:48 +0545

    middleware

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app=express();
 
 app.use(express.json());
 
+app.use((req,res,next)=>{
+  const date =new Date();
+  req.body.date=date;
+  next();
+})
+
 app.use("/",indexRouter);
 
 app.use((err,req,res,next)=>{
